5d856cdae37e47b8f90108f37ef78d6b1f61859a
jsan2321 <jsantiago.c@hotmail.com>
2024-11-05 13:16:28 -0500

    added mvc pattern to student microservice

---
+++ student/src/main/java/com/app/student/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.app.student.controller;
+
+import com.app.student.entity.Student;
+import com.app.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveStudent(@RequestBody Student student){
+        studentService.save(student);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findById(){
+        return ResponseEntity.ok(studentService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+
+    @GetMapping("/search-by-course/{courseId}")
+    public ResponseEntity<?> findByIdCourse(@PathVariable Long courseId){
+        return ResponseEntity.ok(studentService.findByCourseId(courseId));
+    }
+
+}
---
+++ student/src/main/java/com/app/student/service/IStudentService.java
@@ -0,0 +1,17 @@
+package com.app.student.service;
+
+import com.app.student.entity.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+
+    List<Student> findAll();
+
+    Student findById(Long id);
+
+    void save(Student student);
+
+    List<Student> findByIdCourse(Long idCourse);
+
+}
