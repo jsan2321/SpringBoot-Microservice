146386a3386206498cdff5c3e724a0d79588a5ea
jsan2321 <jsantiago.c@hotmail.com>
2024-11-04 23:12:47 -0500

    configured student microservice

--- student/src/main/java/com/app/student/StudentApplication.java
+++ student/src/main/java/com/app/student/StudentApplication.java
@@ -2,7 +2,9 @@ package com.app.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // not necessary
 @SpringBootApplication
 public class StudentApplication {
 
