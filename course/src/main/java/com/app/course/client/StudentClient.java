9b872bcdabc72b02a94a1a6388338e90bbf6f093
jsan2321 <jsantiago.c@hotmail.com>
2024-11-05 17:57:32 -0500

    put all microservices configurations into the configservice

--- course/src/main/java/com/app/course/client/StudentClient.java
+++ course/src/main/java/com/app/course/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090") // microservice to query
+@FeignClient(name = "msvc-student", url = "localhost:8090") // microservice to query ... gateway through port 8080
 public interface StudentClient {
 
     @GetMapping("/api/student/search-by-course/{idCourse}")
